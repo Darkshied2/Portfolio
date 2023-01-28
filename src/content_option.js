f6af0bc0836ff9d4f9fe667b8447079264fb5db4
Darkshied2 <112900012+Darkshied2@users.noreply.github.com>
2023-01-28 21:38:02 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,184 @@
+const logotext = "HARSH KUMAR";
+const meta = {
+    title: "Harsh Kumar",
+    description: "I’m Harsh Kumar app developer _ Full stack developer,currently studing in PSIT,Kanpur",
+};
+
+const introdata = {
+    title: "I’m Harsh Kumar",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+        fourth: "I love develop games",
+    },
+    description: "Behind every successful Coder there an even more successful DeCoder to understand that code..",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "My name is Harsh Kumar. I’m pursuing B.Tech from Pranveer Singh Institute Of Technology,Kanpur. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of web penetration testing . In my free time I like working on open source projects. ",
+};
+const worktimeline = [{
+        jobtitle: "Student",
+        where: "PSIT",
+        date: "2022",
+    },
+    {
+        jobtitle: "Student",
+        where: "PSIT",
+        date: "2022",
+    },
+    {
+        jobtitle: "Student",
+        where: "PSIT",
+        date: "2022",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "C & C++",
+        value: 95,
+    },
+    {
+        name: "Mongodb and Mongoose",
+        value: 75,
+    },
+    {
+        name: "MERN Stack",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 75,
+    },
+    {
+        name: "Jquery",
+        value: 50,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "User experience design (UX design, UXD, UED, or XD) is the process of defining the experience a user would go through when interacting with a digital product or website.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "A mobile application or app is a computer program or software application designed to run on a mobile device .",
+    },
+    {
+        title: "Web Design",
+        description: "Web design is the creation of websites and pages to reflect a company's brand and information and ensure a user-friendly experience. Appearance and design are incorporated as vital elements whether you're designing a website, mobile app or maintaining content on a web page.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "WordPress (WP or WordPress.org) is a free and open-source content management system (CMS) written in hypertext preprocessor language[4] and paired with a MySQL or MariaDB database with supported HTTPS.",
+    },
+    {
+        title: "Game Design",
+        description: "Game design is an exciting, rewarding, and multi-faceted field with promising job prospects. However, becoming a game designer can be a challenging journey. Passion, patience, and persistence are essential !",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/Darkshied2/guess-number-master",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/Darkshied2/guess-number-master",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/Darkshied2/guess-number-master",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/Darkshied2/guess-number-master",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/Darkshied2/guess-number-master",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/Darkshied2/guess-number-master",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/Darkshied2/guess-number-master",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/Darkshied2/guess-number-master",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/Darkshied2/guess-number-master",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/Darkshied2/guess-number-master",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/Darkshied2/guess-number-master",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/Darkshied2/guess-number-master",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "anshraj4558@gmail.com",
+    YOUR_FONE: "(+91)7091762873",
+    description: "The more details about your project you include in your message, the faster I will get back to you. You’ll hear back from me within 2–4 business days.I look forward to hearing from you.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_qiyibu8",
+    YOUR_TEMPLATE_ID: "template_276pqwr",
+    YOUR_USER_ID: "yJ802Hi3PgQi3VKtW",
+};
+
+const socialprofils = {
+    github: "https://github.com/Darkshied2",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/harsh-kumar-71a78b228",
+    twitter: "https://twitter.com",
+    youtube: "https://youtube.com/@harshkumar.7664",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
